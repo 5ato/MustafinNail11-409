1e999d6a3a96317dd7ddf6b00a3ea7674b276ca5
aboba <pomidkil@gmail.com>
2024-11-22 00:29:32 +0300

    Сортировка пузырьком решение

---
+++ Homework/BubbleSort/BubbleSort.cs
@@ -0,0 +1,18 @@
+﻿namespace BubbleSort;
+
+public class BubbleSort
+{
+    public static int[] Solution(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                    (array[j], array[j + 1]) = (array[j + 1], array[j]);
+            }
+        }
+        return array;
+    }
+}
