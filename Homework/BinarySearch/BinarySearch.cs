a61ad61f49f46b1f98abdb603f4ec62f1f7d7412
aboba <pomidkil@gmail.com>
2024-11-22 00:26:05 +0300

    Бинарный поиск решение

---
+++ Homework/BinarySearch/BinarySearch.cs
@@ -0,0 +1,20 @@
+﻿namespace BinarySearch;
+
+public class BinarySearch
+{   
+    public static int Solution(int[] array, int target)
+    {
+        int left = 0, right = array.Length - 1, middle;
+        while (left <= right)
+        {
+            middle = (left + right) / 2;
+            if (array[middle] == target)
+                return middle;
+            else if (array[middle] < target)
+                left = middle + 1;
+            else if (array[middle] > target)
+                right = middle - 1;
+        }
+        return -1;
+    }
+}
