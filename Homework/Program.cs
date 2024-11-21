eb754581eb542693fb0ec8925c6a862077f0b39d
aboba <pomidkil@gmail.com>
2024-11-22 02:40:52 +0300

    correct all wrong things

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Homework;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(BubbleSort.BubbleSort.Solution([1, 5, 4, 1 ,7, 6, 1, 5, 3]));
+        Console.WriteLine(123);
+    }
+
+    public static void PrintArray(string[] array)
+    {
+        foreach (string word in array)
+            Console.WriteLine(word);
+    }
+}
