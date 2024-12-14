de3a3845cb5b209c2acc880b489091f5e837eea3
aboba <pomidkil@gmail.com>
2024-12-14 10:51:04 +0300

    Домашка с описание себя через классы и записи

---
+++ AboutMeClass/Homework.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace AboutMeClass;
+
+public record Person(int BirthYear, string FirstName, string LastName)
+{
+    public int Age
+    {
+        get
+        {
+            return DateTime.Today.Year - BirthYear;
+        }
+    }
+
+    public bool IsAdult
+    {
+        get
+        {
+            return Age >= 18;
+        }
+    }
+
+    public string GetFullName
+    {
+        get
+        {
+            return $"{LastName} {FirstName}";
+        }
+    }
+
+    private readonly int _health = 100;
+}
+
+
+public class StatusPosition;
+
+public class Student(int course, string faculty, int group, bool isForeigner) 
+    : StatusPosition
+{
+    public int Course = course;
+    public string Faculty = faculty;
+    public int Group = group;
+    public bool IsForeigner = isForeigner;
+}
+
+public record Nail(StatusPosition StatusPosition, int BirthYear = 2006, string FirstName = "Наиль", string LastName = "Мустафин") 
+    : Person(BirthYear, FirstName, LastName)
+{
+    public List<string> StackTechno = ["Python", "JavaScript", "HTML", "CSS"];
+    public int GymYearExperience => DateTime.Today.Year - 2022;
+    public string CurrentLanguageProgramm = "C#";
+    public string NotLikedLectionPara = "Матанал";
+}
--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Homework;
+﻿using AboutMeClass;
+
+
+namespace Homework;
 
 class Program
 {
     public static void Main()
     {
-        Console.WriteLine(StringHomework.LongestUniqueSubstring.Solution("abbdefg"));
+        Nail nail = new(new Student(1, "ИТИС", 409, true));
+        Console.WriteLine(nail.Age);
+        Console.WriteLine(nail.FirstName);
+        Console.WriteLine(nail.LastName);
+        Console.WriteLine(nail.GetFullName);
+        Console.WriteLine(nail.CurrentLanguageProgramm);
+        Console.WriteLine(nail.IsAdult);
+        Array.ForEach(nail.StackTechno.ToArray(), Console.Write);
+        Console.WriteLine(nail.GymYearExperience);
+        
     }
 
     public static void PrintArray(string[] array)
